cb19086e13c2dc3ac162a80bfc82b16627cd8e1d
Codrin Popescu <codrin@ascheri.co.uk>
2019-08-05 11:38:34 +0100

    change company path

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create! :email => 'codrin@ascheri.co.uk', 
-					:password => 'Codrin31', 
-					:password_confirmation => 'Codrin31'
-					:admin => true
